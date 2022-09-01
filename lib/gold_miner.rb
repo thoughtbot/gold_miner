01a5eb0a213203e9861b21ae739f99f0cbd664f2
Matheus Richard <matheusrichardt@gmail.com>
2022-08-31 21:45:22 -0300

    Let there be code!

---
+++ lib/gold_miner.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "gold_miner/version"
+
+module GoldMiner
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/gold_miner_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe GoldMiner do
+  it "has a version number" do
+    expect(GoldMiner::VERSION).not_to be nil
+  end
+end
