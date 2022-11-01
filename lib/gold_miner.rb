fa06b845e9085c8711dc9baf9ec59dd02c2652f9
Matheus Richard <matheusrichardt@gmail.com>
2022-11-01 13:58:16 -0300

    Add GoldMiner.mine_in and GoldMiner.convert_messages_to_blogpost

--- lib/gold_miner.rb
+++ lib/gold_miner.rb
@@ -1,9 +1,24 @@
 # frozen_string_literal: true
 
+require "dotenv"
+require_relative "gold_miner/blog_post"
 require_relative "gold_miner/helpers"
 require_relative "gold_miner/messages_query"
 require_relative "gold_miner/slack_client"
 require_relative "gold_miner/version"
 
 module GoldMiner
+  extend self
+
+  def mine_in(slack_channel, slack_client: GoldMiner::SlackClient, env_file: ".env")
+    Dotenv.load(env_file)
+
+    slack_client
+      .build(api_token: ENV["SLACK_API_TOKEN"])
+      .fmap { |client| client.search_interesting_messages_in(slack_channel) }
+  end
+
+  def convert_messages_to_blogpost(channel, messages, blog_post_builder: GoldMiner::BlogPost)
+    blog_post_builder.new(slack_channel: channel, messages: messages, since: Helpers::Time.last_friday)
+  end
 end
--- spec/gold_miner_spec.rb
+++ spec/gold_miner_spec.rb
@@ -1,6 +1,54 @@
 # frozen_string_literal: true
 
 RSpec.describe GoldMiner do
+  include Dry::Monads[:result]
+
+  describe ".mine_in" do
+    it "loads the API token from the given env file" do
+      slack_client_builder = spy("SlackClient builder")
+
+      GoldMiner.mine_in("dev", slack_client: slack_client_builder, env_file: "./spec/fixtures/.env")
+
+      expect(slack_client_builder).to have_received(:build).with(api_token: "test-token")
+    end
+
+    it "returns interesting messages from the given channel" do
+      messages = {text: "text", author_username: "user1", permalink: "http://permalink-1.com"}
+      slack_client = instance_double(GoldMiner::SlackClient, search_interesting_messages_in: messages)
+      slack_client_builder = double(GoldMiner::SlackClient, build: Success(slack_client))
+
+      result = GoldMiner.mine_in("dev", slack_client: slack_client_builder, env_file: "./spec/fixtures/.env")
+
+      expect(result.value!).to eq messages
+    end
+  end
+
+  describe ".convert_messages_to_blogpost" do
+    it "converts slack messages to a blogpost and writes it to a file" do
+      travel_to "2022-10-07" do
+        channel = "dev"
+        messages = [
+          {text: "text", author_username: "user1", permalink: "http://permalink-1.com"},
+          {text: "text2", author_username: "user2", permalink: "http://permalink-2.com"}
+        ]
+        blog_post_builder = spy("BlogPost builder")
+
+        GoldMiner.convert_messages_to_blogpost(channel, messages, blog_post_builder: blog_post_builder)
+
+        expect(blog_post_builder).to have_received(:new).with(slack_channel: channel, messages: messages, since: "2022-09-30")
+      end
+    end
+  end
+
+  it "keeps env files in sync" do
+    fixture_env = Dotenv.parse("./spec/fixtures/.env")
+    example_env = Dotenv.parse(".env.example")
+    env = Dotenv.parse(".env")
+
+    expect(env.keys).to match_array(example_env.keys)
+    expect(env.keys).to match_array(fixture_env.keys)
+  end
+
   it "has a version number" do
     expect(GoldMiner::VERSION).not_to be nil
   end
