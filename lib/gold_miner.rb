6d1636784cf446f1eb8b71577a66d292d30774de
Matheus Richard <matheusrichardt@gmail.com>
2022-11-01 13:54:13 -0300

    Import helper module

--- lib/gold_miner.rb
+++ lib/gold_miner.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
-require_relative "gold_miner/version"
-require_relative "gold_miner/slack_client"
+require_relative "gold_miner/helpers"
 require_relative "gold_miner/messages_query"
+require_relative "gold_miner/slack_client"
+require_relative "gold_miner/version"
 
 module GoldMiner
 end
