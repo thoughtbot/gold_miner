b017d83aeaef86a58a180e42e5b191e2cb977ff7
Matheus Richard <matheusrichardt@gmail.com>
2022-12-22 13:20:08 -0300

    Search messages with gold emoji
    This adds a way to manually mark a message to be picked by GoldMiner.
    In the future, we might keep only the "golden messages" query, and
    delete the TIL and tip queries (as they might be redundant).
    
    This partially covers
    https://github.com/thoughtbot/gold_miner/issues/2

--- lib/gold_miner/slack_client.rb
+++ lib/gold_miner/slack_client.rb
@@ -5,6 +5,8 @@ require "slack-ruby-client"
 
 module GoldMiner
   class SlackClient
+    GOLD_EMOJI = "rupee-gold"
+
     extend Dry::Monads[:result]
 
     def self.build(api_token:)
@@ -36,8 +38,12 @@ module GoldMiner
         @slack.search_messages(query: interesting_messages_query(channel).with_topic("tip")),
         topic: :tip
       )
+      golden_messages = extract_messages_from_result(
+        @slack.search_messages(query: interesting_messages_query(channel).with_reaction(GOLD_EMOJI)),
+        topic: nil
+      )
 
-      (til_messages + tip_messages).uniq { |message| message[:permalink] }
+      (til_messages + tip_messages + golden_messages).uniq { |message| message[:permalink] }
     end
 
     private_class_method :new
