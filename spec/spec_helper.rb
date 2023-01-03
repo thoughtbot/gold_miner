914d9d9939644b5353b1c1b524bf287c326cc625
Matheus Richard <matheusrichardt@gmail.com>
2023-01-03 10:28:12 -0300

    Allow focusing specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,9 @@ RSpec.configure do |config|
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
+  # Enable RSpec focus
+  config.filter_run_when_matching :focus
+
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
