cdfe56a7acd080760516e3c8c7e5bbf97509fdbb
Matheus Richard <matheusrichardt@gmail.com>
2023-08-11 12:36:38 -0300

    Fix specs

--- spec/gold_miner/slack/message_spec.rb
+++ spec/gold_miner/slack/message_spec.rb
@@ -6,29 +6,31 @@ require "ostruct"
 RSpec.describe GoldMiner::Slack::Message do
   describe "#[]" do
     it "returns the message attribute" do
+      author = GoldMiner::Slack::User.new(id: "U123", name: "John Doe", username: "johndoe")
       message = described_class.new(
         text: "TIL",
-        author: "@JohnDoe",
+        author: author,
         permalink: "https:///message-1-permalink.com"
       )
 
       expect(message[:text]).to eq "TIL"
-      expect(message[:author]).to eq "@JohnDoe"
+      expect(message[:author]).to eq author
       expect(message[:permalink]).to eq "https:///message-1-permalink.com"
     end
   end
 
   describe "#as_conversation" do
     it "returns the message content with the author name" do
+      author = GoldMiner::Slack::User.new(id: "U123", name: "John Doe", username: "johndoe")
       message = described_class.new(
         text: "TIL",
-        author: "@JohnDoe",
+        author: author,
         permalink: "https:///message-1-permalink.com"
       )
 
       conversation = message.as_conversation
 
-      expect(conversation).to eq "@JohnDoe says: TIL"
+      expect(conversation).to eq "John Doe says: TIL"
     end
   end
 end
