9a9bbb550401a45f3b4c5067869ad120fae0084b
Matheus Richard <matheusrichardt@gmail.com>
2023-08-11 11:12:37 -0300

    Remove unnecessary test
    This is already guaranteed by the `Data` class.

--- spec/gold_miner/slack/message_spec.rb
+++ spec/gold_miner/slack/message_spec.rb
@@ -18,55 +18,6 @@ RSpec.describe GoldMiner::Slack::Message do
     end
   end
 
-  describe "#==" do
-    it "returns true if the messages have the same attributes" do
-      message1 = described_class.new(
-        text: "TIL",
-        author: "@JohnDoe",
-        permalink: "https:///message-1-permalink.com"
-      )
-      message2 = described_class.new(
-        text: "TIL",
-        author: "@JohnDoe",
-        permalink: "https:///message-1-permalink.com"
-      )
-
-      result = message1 == message2
-
-      expect(result).to be true
-    end
-
-    it "returns false if any of the attributes are different" do
-      message1 = described_class.new(
-        text: "TIL",
-        author: "@JohnDoe",
-        permalink: "https:///message-1-permalink.com"
-      )
-      message2 = described_class.new(
-        text: "TIL2",
-        author: "@JohnDoe",
-        permalink: "https:///message-1-permalink.com"
-      )
-      message3 = described_class.new(
-        text: "TIL",
-        author: "@JohnDoe2",
-        permalink: "https:///message-1-permalink.com"
-      )
-      message4 = described_class.new(
-        text: "TIL",
-        author: "@JohnDoe",
-        permalink: "https:///message-2-permalink.com"
-      )
-
-      expect(message1 == message2).to be false
-      expect(message1 == message3).to be false
-      expect(message1 == message4).to be false
-      expect(message2 == message3).to be false
-      expect(message2 == message4).to be false
-      expect(message3 == message4).to be false
-    end
-  end
-
   describe "#as_conversation" do
     it "returns the message content with the author name" do
       message = described_class.new(
