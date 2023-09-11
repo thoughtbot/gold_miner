326d3e2ace6bc36c4d926d036d0f87b30b3b469d
Matheus Richard <matheusrichardt@gmail.com>
2023-09-10 22:08:13 -0300

    Fix OpenAiWritter

--- lib/gold_miner/blog_post/open_ai_writer.rb
+++ lib/gold_miner/blog_post/open_ai_writer.rb
@@ -10,35 +10,35 @@ module GoldMiner
         @fallback_writer = fallback_writer
       end
 
-      def extract_topics_from(message)
-        topics_json = ask_openai("Extract the 3 most relevant topics, if possible in one word, from this text as a single parseable JSON array: #{message[:text]}")
+      def extract_topics_from(gold_nugget)
+        topics_json = ask_openai("Extract the 3 most relevant topics, if possible in one word, from this text as a single parseable JSON array: #{gold_nugget.content}")
 
         if (topics = try_parse_json(topics_json))
           topics
         else
-          fallback_topics_for(message)
+          fallback_topics_for(gold_nugget)
         end
       end
 
-      def give_title_to(message)
-        title = ask_openai("Give a small title to this text: #{message[:text]}")
+      def give_title_to(gold_nugget)
+        title = ask_openai("Give a small title to this text: #{gold_nugget.content}")
         title = title&.delete_prefix('"')&.delete_suffix('"')
 
-        title || fallback_title_for(message)
+        title || fallback_title_for(gold_nugget)
       end
 
-      def summarize(message)
+      def summarize(gold_nugget)
         summary = ask_openai <<~PROMPT
           Summarize the following markdown message without removing the author's blog link. Return the summary as markdown.
 
           Message:
-          #{message.as_conversation}
+          #{gold_nugget.as_conversation}
         PROMPT
 
         if summary
-          "#{summary}\n\nSource: #{message[:permalink]}"
+          "#{summary}\n\nSource: #{gold_nugget.source}"
         else
-          fallback_summary_for(message)
+          fallback_summary_for(gold_nugget)
         end
       end
 
@@ -63,16 +63,16 @@ module GoldMiner
         nil
       end
 
-      def fallback_title_for(message)
-        @fallback_writer.give_title_to(message)
+      def fallback_title_for(gold_nugget)
+        @fallback_writer.give_title_to(gold_nugget)
       end
 
-      def fallback_summary_for(message)
-        @fallback_writer.summarize(message)
+      def fallback_summary_for(gold_nugget)
+        @fallback_writer.summarize(gold_nugget)
       end
 
-      def fallback_topics_for(message)
-        @fallback_writer.extract_topics_from(message)
+      def fallback_topics_for(gold_nugget)
+        @fallback_writer.extract_topics_from(gold_nugget)
       end
 
       def try_parse_json(json)
--- lib/gold_miner/gold_nugget.rb
+++ lib/gold_miner/gold_nugget.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 module GoldMiner
-  GoldNugget = Data.define(:content, :author, :source)
+  GoldNugget = Data.define(:content, :author, :source) do
+    def as_conversation
+      <<~MARKDOWN
+        #{author.name_with_link_reference} says: #{content}
+
+        #{author.reference_link}
+      MARKDOWN
+    end
+  end
 end
