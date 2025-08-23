f7a988f5909a2caef236400d64982dc3b6c43cb8
Matheus Richard <matheusrichardt@gmail.com>
2025-08-22 22:01:59 -0300

    Typo

--- spec/support/test_factories.rb
+++ spec/support/test_factories.rb
@@ -1,59 +1,59 @@
 module TestFactories
   extend self
 
-  def create_author(overriden_attributes = {})
+  def create_author(overridden_attributes = {})
     default_attributes = {
       id: "author-id",
       name: "John Doe",
       link: "https://example.com/users/john.doe"
     }
-    GoldMiner::Author.new(**default_attributes.merge(overriden_attributes))
+    GoldMiner::Author.new(**default_attributes.merge(overridden_attributes))
   end
 
-  def create_gold_container(overriden_attributes = {})
+  def create_gold_container(overridden_attributes = {})
     default_attributes = {
       gold_nuggets: [],
       origin: "some-channel",
       packing_date: Date.today
     }
-    GoldMiner::GoldContainer.new(**default_attributes.merge(overriden_attributes))
+    GoldMiner::GoldContainer.new(**default_attributes.merge(overridden_attributes))
   end
 
-  def create_gold_nugget(overriden_attributes = {})
+  def create_gold_nugget(overridden_attributes = {})
     default_attributes = {
       content: "TIL about the difference betweeen .size and .count in Rails",
       source: "https://example.com/messages/1",
       author: create_author
     }
-    GoldMiner::GoldNugget.new(**default_attributes.merge(overriden_attributes))
+    GoldMiner::GoldNugget.new(**default_attributes.merge(overridden_attributes))
   end
 
-  def create_blog_post(overriden_attributes = {})
+  def create_blog_post(overridden_attributes = {})
     default_attributes = {
       slack_channel: "design",
       gold_nuggets: [create_gold_nugget],
       since: Date.today
     }
 
-    GoldMiner::BlogPost.new(**default_attributes.merge(overriden_attributes))
+    GoldMiner::BlogPost.new(**default_attributes.merge(overridden_attributes))
   end
 
-  def create_slack_user(overriden_attributes = {})
+  def create_slack_user(overridden_attributes = {})
     default_attributes = {
       id: "U123",
       name: "John Doe",
       username: "john.doe"
     }
-    GoldMiner::Slack::User.new(**default_attributes.merge(overriden_attributes))
+    GoldMiner::Slack::User.new(**default_attributes.merge(overridden_attributes))
   end
 
-  def create_slack_message(overriden_attributes = {})
+  def create_slack_message(overridden_attributes = {})
     default_attributes = {
       id: "msg-id",
       text: "Hello world",
       user: create_slack_user,
       permalink: "https://example.com/slack/messages/123"
     }
-    GoldMiner::Slack::Message.new(**default_attributes.merge(overriden_attributes))
+    GoldMiner::Slack::Message.new(**default_attributes.merge(overridden_attributes))
   end
 end
